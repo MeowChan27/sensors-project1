c142e69c33648e3c545e4d03e3bbbbf59dfb81f4
Prasanaa Vingadassamy <110774433+MeowChan27@users.noreply.github.com>
2022-08-17 11:18:18 +0200

    Add files via upload

---
+++ init_db.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO atmosphere (time, temperature, humidite, pression) VALUES (?, ?, ?, ?)",
+            ('10h00', 15, '40%', 1.3)
+            )
+cur.execute("INSERT INTO atmosphere (time, temperature,humidite,pression) VALUES (?, ?, ?,?)",
+            ('10h20', 16, '50%', 1.4)
+            )
+
+connection.commit()
+connection.close()
---
+++ schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS atmosphere;
+
+CREATE TABLE atmosphere(
+    time varchar(5),
+    temperature INT,
+    humidite varchar(3),
+    pression decimal(2,1)
+);
