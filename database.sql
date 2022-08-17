117881172f20a11f53ca9745c7b61e3f54ee398b
Prasanaa Vingadassamy <110774433+MeowChan27@users.noreply.github.com>
2022-08-17 11:16:33 +0200

    Delete database.sql

--- database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS 'datatest'
-USE 'datatest'
-
-CREATE TABLE atmosphere(
-    time varchar(5),
-    temperature INT,
-    humidite varchar(3),
-    pressure decimal(2,1)
-);
